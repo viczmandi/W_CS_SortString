99ac95773ecc03f4510dead12963146af5968927
Viczmándi Gergely <viczmandi@gmail.com>
2016-04-02 22:43:24 +0200

    First commit/push

---
+++ SortString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] sa = s.Split();
+
+            Array.Sort(sa);
+
+            s = string.Join(" ", sa);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
